a5b6ee0ada0678b1ba5e96eae527875cb5b33c1b
Nalin Parihar <nalin7parihar@gmail.com>
2025-03-17 22:31:42 +0530

    Google auth working

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCpO10HQzSialXNoVhSeVqGFK30ar1O4wU",
-  authDomain: "rideshare-76394.firebaseapp.com",
-  projectId: "rideshare-76394",
-  storageBucket: "rideshare-76394.firebasestorage.app",
-  messagingSenderId: "431164031640",
-  appId: "1:431164031640:web:bd11e89b3f57b8082fc6ed",
-  measurementId: "G-KGCZ97SZF0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
