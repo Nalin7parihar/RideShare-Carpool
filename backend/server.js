826234342a09e8e5cd5a9e95d250e2f7f5f8ff13
Nalin Parihar <nalin7parihar@gmail.com>
2025-04-05 22:20:21 +0530

    done cors right

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin :  ["http://localhost:5173"],
+  origin :  ["http://localhost:5173","https://rideshare-orcin-eight.vercel.app/Auth"],
   credentials : true
 }));
 
